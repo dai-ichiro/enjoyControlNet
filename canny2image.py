6612ed86234bce66b769459b771bd8e66f622e06
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-02-18 13:37:54 +0900

    Add files via upload

--- canny2image.py
+++ canny2image.py
@@ -1,18 +1,15 @@
 from share import *
-import config
 
-import cv2
 import einops
-import gradio as gr
 import numpy as np
 import torch
-import random
 
 from pytorch_lightning import seed_everything
 from annotator.util import resize_image, HWC3
 from annotator.canny import CannyDetector
 from cldm.model import create_model, load_state_dict
 from ldm.models.diffusion.ddim import DDIMSampler
+import os
 from PIL import Image
 
 from argparse import ArgumentParser
@@ -30,9 +27,9 @@ parser.add_argument(
     help='the seed (for reproducible sampling)',
 )
 parser.add_argument(
-    '--num_samples',
+    '--n_samples',
     type=int,
-    default=1,
+    default=5,
     help='number of samples',
 )
 parser.add_argument(
@@ -47,17 +44,30 @@ parser.add_argument(
     default=9.0,
     help='Guidance Scale',
 )
+parser.add_argument(
+    '--low_threshold',
+    type=int,
+    default=100,
+    help='Canny low threshold(min=1, max=255)',
+)
+parser.add_argument(
+    '--high_threshold',
+    type=int,
+    default=200,
+    help='Canny high threshold(min=1, max=255)',
+)
 args = parser.parse_args()
 
 original_image = np.array(Image.open(args.image))
 seed = args.seed
-num_samples = args.num_samples
+n_samples = args.n_samples
 steps = args.steps
 scale = args.scale
+low_threshold = args.low_threshold
+high_threshold = args.high_threshold
 
 image_resolution = 512
-low_threshold = 100
-high_threshold = 200
+eta = 0.0
 
 apply_canny = CannyDetector()
 
@@ -69,8 +79,8 @@ ddim_sampler = DDIMSampler(model)
 a_prompt = 'best quality, extremely detailed'
 n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality'
 
-def process(prompt, eta):
-    
+def main(prompt):
+    num_samples = 1
     with torch.no_grad():
         img = resize_image(HWC3(original_image), image_resolution)
         H, W, C = img.shape
@@ -78,63 +88,32 @@ def process(prompt, eta):
         detected_map = apply_canny(img, low_threshold, high_threshold)
         detected_map = HWC3(detected_map)
 
+        pil_image_detected_map = Image.fromarray(255 - detected_map)
+        pil_image_detected_map.save(os.path.join('results', 'detected_map.png'))
+
         control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
         control = torch.stack([control for _ in range(num_samples)], dim=0)
         control = einops.rearrange(control, 'b h w c -> b c h w').clone()
-
-        seed_everything(seed)
-
-        if config.save_memory:
-            model.low_vram_shift(is_diffusing=False)
 
         cond = {"c_concat": [control], "c_crossattn": [model.get_learned_conditioning([prompt + ', ' + a_prompt] * num_samples)]}
         un_cond = {"c_concat": [control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * num_samples)]}
         shape = (4, H // 8, W // 8)
 
-        if config.save_memory:
-            model.low_vram_shift(is_diffusing=True)
 
-        samples, intermediates = ddim_sampler.sample(steps, num_samples,
-                                                     shape, cond, verbose=False, eta=eta,
-                                                     unconditional_guidance_scale=scale,
-                                                     unconditional_conditioning=un_cond)
+        for i in range(n_samples):
+            temp_seed = seed + i * 1000
+            seed_everything(temp_seed)
+            samples, intermediates = ddim_sampler.sample(steps, num_samples,
+                                                        shape, cond, verbose=False, eta=eta,
+                                                        unconditional_guidance_scale=scale,
+                                                        unconditional_conditioning=un_cond)
 
-        if config.save_memory:
-            model.low_vram_shift(is_diffusing=False)
+            x_samples = model.decode_first_stage(samples)
+            x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
 
-        x_samples = model.decode_first_stage(samples)
-        x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
+            results = Image.fromarray(x_samples[0])
+            results.save(os.path.join('results', f'result_seed{temp_seed}.png'))
 
-        results = [x_samples[i] for i in range(num_samples)]
-    return [255 - detected_map] + results
-
-
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Control Stable Diffusion with Canny Edge Maps")
-    with gr.Row():
-        with gr.Column():
-            input_image = gr.Image(source='upload', type="numpy")
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            with gr.Accordion("Advanced options", open=False):
-                #num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-                #image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=256)
-                #low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
-                #high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
-                #ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
-                #scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                #seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, randomize=True)
-                eta = gr.Number(label="eta (DDIM)", value=0.0)
-                #a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
-                #n_prompt = gr.Textbox(label="Negative Prompt",
-                                      #value='longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality')
-        with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    #ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, scale, seed, eta, low_threshold, high_threshold]
-    ips = [prompt, eta]
-    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
-
-
-block.launch(server_name='localhost')
+if __name__ == '__main__':
+    os.makedirs('results', exist_ok=True)
+    main('')
