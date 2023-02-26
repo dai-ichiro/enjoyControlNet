830db22dca99492f12efbe47f519fe76c5fba492
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-02-26 10:20:10 +0900

    Add files via upload

--- diffusers/dev_canny2image.py
+++ diffusers/dev_canny2image.py
@@ -2,12 +2,31 @@ import os
 os.makedirs('results', exist_ok=True)
 
 import numpy as np
+import torch
 from diffusers import StableDiffusionControlNetPipeline, AutoencoderKL, UNet2DConditionModel, EulerAncestralDiscreteScheduler
 from diffusers.utils import load_image
 import controlnet_hinter
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--model',
+    type=str,
+    required=True,
+    help='model',
+)
+parser.add_argument(
+    '--seed',
+    type=int,
+    default=20000,
+    help='the seed (for reproducible sampling)',
+)
+parser.add_argument(
+    '--n_samples',
+    type=int,
+    default=1,
+    help='how many samples to produce for each given prompt',
+)
 parser.add_argument(
     '--image',
     type=str,
@@ -37,11 +56,22 @@ parser.add_argument(
     type=int,
     help='num_inference_steps',
 )
+parser.add_argument(
+    '--vae',
+    type=str,
+    help='vae'
+)
 args = parser.parse_args()
 
+seed = args.seed
 steps = args.steps
+
 threshold1 = args.threshold1
 threshold2 = args.threshold2
+
+vae_folder =args.vae
+
+base_model_id = args.model
 
 if args.from_canny:
     control = load_image(args.image)
@@ -49,8 +79,11 @@ else:
     control = controlnet_hinter.hint_canny(np.array(load_image(args.image)), low_threshold=threshold1, high_threshold=threshold2)
     control.save(os.path.join('results', 'canny.png'))
 
-base_model_id = 'Counterfeit-V2.5'  # an example: openjourney model
-vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae').to('cuda')
+if vae_folder is not None:
+    vae = AutoencoderKL.from_pretrained(vae_folder).to('cuda')
+else:
+    vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae').to('cuda')
+
 unet = UNet2DConditionModel.from_pretrained(base_model_id, subfolder='unet').to('cuda')
 
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
@@ -59,13 +92,19 @@ pipe = StableDiffusionControlNetPipeline.from_pretrained(
     vae=vae,
     safety_checker=None).to('cuda')
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+pipe.enable_xformers_memory_efficient_attention()
 
-image = pipe(
-    prompt='best quality, extremely detailed',  
-    negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality",
-    image=control,
-    width=512,
-    height=512,
-    num_inference_steps=steps).images[0]
+for i in range(args.n_samples):
+    seed_i = seed + i * 1000
+    generator = torch.manual_seed(seed_i)
+    image = pipe(
+        prompt="best quality, extremely detailed", 
+        negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality",
+        image=control,
+        width = 512,
+        height = 512,
+        num_inference_steps=steps, 
+        generator=generator).images[0]
 
-image.save(os.path.join('results', 'generated.png'))
+    image.save(os.path.join('results', f'seed{seed_i}.png'))
+    
