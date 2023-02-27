b577c73c8749872b06395f5973186a9a6d643023
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-02-27 12:15:44 +0900

    update

--- diffusers/hinter_canny.py
+++ diffusers/hinter_canny.py
@@ -12,6 +12,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 original_image = np.array(load_image(args.image))
+height, width, _ = original_image.shape
 
 threshold1_list = [50, 100, 150, 200, 250, 300, 350, 400]
 
@@ -21,5 +22,9 @@ os.makedirs('results', exist_ok=True)
 for threshold1 in threshold1_list:
     threshold2_list = [x for x in threshold1_list if x >= threshold1]
     for threshold2 in threshold2_list:
-        control = controlnet_hinter.hint_canny(original_image, low_threshold=threshold1, high_threshold=threshold2)
+        control = controlnet_hinter.hint_canny(
+            original_image, 
+            low_threshold=threshold1, high_threshold=threshold2,
+            width=width, height=height)
+        
         control.save(os.path.join('results', f'{threshold1}_{threshold2}.png'))
