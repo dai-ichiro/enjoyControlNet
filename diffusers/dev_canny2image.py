7be3ca7bc2eb6fdb49c8338f1c0049725f5c8bf2
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-02 12:20:55 +0900

    Update dev_canny2image.py

--- diffusers/dev_canny2image.py
+++ diffusers/dev_canny2image.py
@@ -96,7 +96,7 @@ vae_folder =args.vae
 base_model_id = args.model
 
 if args.from_canny:
-    control = load_image(args.image)
+    control = load_image(args.image).resize((resolution, resolution))
 else:
     control = controlnet_hinter.hint_canny(
         np.array(load_image(args.image)), 
