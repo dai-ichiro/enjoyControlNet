0ab21a1fb1c8dd7b97996fd276ef00f5b8f5fa45
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-02 15:42:45 +0900

    Add files via upload

--- diffusers/tools/hinter_canny.py
+++ diffusers/tools/hinter_canny.py
@@ -10,21 +10,36 @@ parser.add_argument(
     type=str,
     help='original image'
 )
+parser.add_argument(
+    '--resolution',
+    type=int,
+    help='resolution'
+)
+
 args = parser.parse_args()
 original_image = np.array(load_image(args.image))
 height, width, _ = original_image.shape
 
-threshold1_list = [50, 100, 150, 200, 250, 300, 350, 400]
+if height != width:
+    print('need square image')
+    import sys
+    sys.exit()
+
+if args.resolution is not None:
+    resolution = args.resolution
+else:
+    resolution = width
+
+threshold1_list = [25, 50, 100, 150, 200, 250]
 
 import os
 os.makedirs('canny_results', exist_ok=True)
 
 for threshold1 in threshold1_list:
-    threshold2_list = [x for x in threshold1_list if x >= threshold1]
-    for threshold2 in threshold2_list:
+    for threshold2 in threshold1_list:
         control = controlnet_hinter.hint_canny(
             original_image, 
             low_threshold=threshold1, high_threshold=threshold2,
-            width=width, height=height)
+            width=resolution, height=resolution)
         
         control.save(os.path.join('canny_results', f'{threshold1}_{threshold2}.png'))
