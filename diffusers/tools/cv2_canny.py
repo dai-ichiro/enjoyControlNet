d74c476b5b4b0f638adfcba39aead4b6d1a989c5
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-07 23:10:52 +0900

    Add files via upload

--- diffusers/tools/cv2_canny.py
+++ diffusers/tools/cv2_canny.py
@@ -10,14 +10,31 @@ parser.add_argument(
     type=str,
     help='original image'
 )
+parser.add_argument(
+    '--W',
+    type=int,
+    default=512,
+    help='width'
+)
+parser.add_argument(
+    '--H',
+    type=int,
+    default=512,
+    help='height'
+)
+
 args = parser.parse_args()
-original_image = np.array(Image.open(args.image))
 
-threshold1_list = [50, 100, 150, 200, 250, 300, 350, 400]
+width = args.W
+height = args.H
 
-os.makedirs('results', exist_ok=True)
+original_image = np.array(Image.open(args.image).resize((width, height)))
+
+threshold1_list = [50, 100, 150, 200]
+
+os.makedirs('canny_results', exist_ok=True)
 for threshold1 in threshold1_list:
     threshold2_list = [x for x in threshold1_list if x >= threshold1]
     for threshold2 in threshold2_list:
         control = cv2.Canny(original_image, threshold1=threshold1, threshold2=threshold2)
-        Image.fromarray(control).save(os.path.join('results', f'{threshold1}_{threshold2}.png'))
+        Image.fromarray(control).save(os.path.join('canny_results', f'{threshold1}_{threshold2}.png'))
