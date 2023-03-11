40dcf6abfc617e5ec276bb167a0dc878202a50f9
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-11 20:35:06 +0900

    add 'all' argument

--- controlnet_aux/scribble.py
+++ controlnet_aux/scribble.py
@@ -4,8 +4,6 @@ from PIL import Image
 import argparse
 import os 
 
-os.makedirs('sketch_results', exist_ok=True)
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--threshold_list',
@@ -25,6 +23,11 @@ parser.add_argument(
     type=int,
     help='resolution'
 )
+parser.add_argument(
+    '--all',
+    action='store_true',
+    help='if true, threshold from 0.1 to 0.9'
+)
 opt = parser.parse_args()
 
 resolution = opt.resolution
@@ -38,10 +41,17 @@ else:
 hed = HEDdetector.from_pretrained('lllyasviel/ControlNet')
 hed_array = np.array(hed(image, detect_resolution=resolution, image_resolution=resolution).convert('L'))
 
-for threshold in opt.threshold_list:
+if opt.all:
+    threshold_list = [x * 0.1 for x in range(1, 10)]
+else:
+    threshold_list = opt.threshold
+
+os.makedirs('scribble_results', exist_ok=True)
+
+for threshold in threshold_list:
     bool_array = hed_array > (255 * threshold)
     result_array = np.where(bool_array == False, 0, 255).astype(np.uint8)
 
     pil = Image.fromarray(result_array, mode='L')
-    pil.save(os.path.join('sketch_results', f'sketch_{threshold}.png'))
+    pil.save(os.path.join('scribble_results', f'sketch{threshold:.1f}.png'))
 
