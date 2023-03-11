651fecf6f1fdf6a9fdc7f78f104063c9798e016d
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-11 17:45:54 +0900

    Add files via upload

--- diffusers/tools/sketch.py
+++ diffusers/tools/sketch.py
@@ -11,14 +11,23 @@ parser.add_argument(
 parser.add_argument(
     '--size',
     type=int,
-    default=4,
+    default=8,
     help='rectangle size'
 )
+parser.add_argument(
+    '--color',
+    type=str,
+    default='black',
+    choices=['black', 'white'],
+    help='line color'
+)
+
 
 def nothing(x):
     pass
 
 opt = parser.parse_args()
+color = opt.color
 size = opt.size
 
 img_path = opt.image
@@ -30,9 +39,6 @@ def draw(event,x,y, flags, param):
 
     global drawing, mask_image
 
-    s = cv2.getTrackbarPos('Size','image') * 3 + 3
-    i = cv2.getTrackbarPos(switch,'image')
-
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
 
@@ -40,14 +46,14 @@ def draw(event,x,y, flags, param):
 
         if drawing == True:
             # cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, thickness)
-            xmin = x - s
-            ymin = y - s
-            xmax = x + s
-            ymax = y + s
+            xmin = x - size
+            ymin = y - size
+            xmax = x + size
+            ymax = y + size
 
-            if i == 0:
+            if color=='white':
                 cv2.rectangle(original_image, (xmin,ymin), (xmax, ymax), 255, -1)
-            elif i == 1:
+            elif color=='black':
                 cv2.rectangle(original_image, (xmin,ymin), (xmax, ymax), 0, -1)
 
     elif event == cv2.EVENT_LBUTTONUP:
@@ -55,16 +61,25 @@ def draw(event,x,y, flags, param):
         
 original_image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-cv2.namedWindow('image')
-switch = 'draw / erase'
-cv2.createTrackbar('Size', 'image', 0, 2,nothing)
-cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+width, height = original_image.shape[0:2]
+position_x = 100
+position_y = 100
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', width, height)
+cv2.moveWindow('image', position_x, position_y)
 cv2.setMouseCallback('image', draw)
 
 while True:  
     k = cv2.waitKey(1) & 0xFF
     if k == ord('s'):
         cv2.imwrite(f'{img_fname_no_ext}_modified.png', original_image)
+    elif k == ord('u'):
+        position_y -= 200
+        cv2.moveWindow('image', position_x, position_y)
+    elif k == ord('d'):
+        position_y += 200
+        cv2.moveWindow('image', position_x, position_y)
     elif k & 0xFF == 27:
         break
     cv2.imshow('image',original_image)
