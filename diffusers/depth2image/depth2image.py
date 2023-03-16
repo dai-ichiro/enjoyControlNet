6b136044a2668daf0ae73a0c3cd304667c551060
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-16 16:35:31 +0900

    Add files via upload

--- diffusers/depth2image/depth2image.py
+++ diffusers/depth2image/depth2image.py
@@ -3,12 +3,16 @@ os.makedirs('results', exist_ok=True)
 
 import numpy as np
 import torch
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, EulerAncestralDiscreteScheduler, AutoencoderKL
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, AutoencoderKL
 from diffusers.utils import load_image
-import controlnet_hinter
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--controlnet',
+    type=str,
+    default='controlnet/sd-controlnet-canny'
+)
 parser.add_argument(
     '--model',
     type=str,
@@ -56,6 +60,17 @@ parser.add_argument(
     type=str,
     help='vae'
 )
+parser.add_argument(
+    '--prompt',
+    type=str,
+    help='prompt'
+)
+parser.add_argument(
+    '--scheduler',
+    type=str,
+    default='pndm',
+    choices=['pndm', 'multistepdpm', 'eulera']
+)
 args = parser.parse_args()
 
 seed = args.seed
@@ -63,36 +78,70 @@ steps = args.steps
 scale_list = args.scale
 
 vae_folder =args.vae
+control_model_id = args.controlnet
 base_model_id = args.model
 
-if args.from_scribble:
+n_samples = args.n_samples
+
+if args.from_depth:
     controlhint = load_image(args.image)
 else:
     #todo 
     controlhint = load_image(args.image)
 
 if vae_folder is not None:
-    vae = AutoencoderKL.from_pretrained(vae_folder).to('cuda')
+    vae = AutoencoderKL.from_pretrained(vae_folder, torch_dtype=torch.float16).to('cuda')
 else:
-    vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae').to('cuda')
+    vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae', torch_dtype=torch.float16).to('cuda')
 
-controlnet = ControlNetModel.from_pretrained("basemodel/sd-controlnet-scribble")
+controlnet = ControlNetModel.from_pretrained(control_model_id, torch_dtype=torch.float16)
 
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
     base_model_id,
     controlnet=controlnet,
     vae=vae,
-    safety_checker=None).to('cuda')
-pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    safety_checker=None,
+    torch_dtype=torch.float16).to('cuda')
+
+scheduler = args.scheduler
+match scheduler:
+    case 'pmdn':
+        from diffusers import  PNDMScheduler
+        pipe.scheduler = PNDMScheduler.from_config(pipe.scheduler.config)
+    case 'multistepdpm':
+        from diffusers import DPMSolverMultistepScheduler
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    case 'eulera':
+        from diffusers import EulerAncestralDiscreteScheduler
+        pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    case _:
+        None
+
 pipe.enable_xformers_memory_efficient_attention()
 
-for i in range(args.n_samples):
+if args.prompt is not None and os.path.isfile(args.prompt):
+    print(f'reading prompts from {args.prompt}')
+    with open(args.prompt, 'r') as f:
+        prompt_from_file = f.readlines()
+        prompt_from_file = [x.strip() for x in prompt_from_file if x.strip() != '']
+        prompt_from_file = ', '.join(prompt_from_file)
+        prompt = f'{prompt_from_file}, best quality, extremely detailed'
+else:
+    prompt = 'best quality, extremely detailed'
+
+negative_prompt = 'monochrome, lowres, bad anatomy, worst quality, low quality'
+
+print(f'n_samples: {n_samples}')
+print(f'prompt: {prompt}')
+print(f'negative prompt: {negative_prompt}')
+
+for i in range(n_samples):
     seed_i = seed + i * 1000
     for scale in scale_list:
         generator = torch.manual_seed(seed_i)
         image = pipe(
-            prompt="best quality, extremely detailed", 
-            negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality",
+            prompt=prompt, 
+            negative_prompt=negative_prompt,
             image=controlhint,
             num_inference_steps=steps, 
             generator=generator,
