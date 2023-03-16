0ba9d34ea9f1f1365761e3eb2a806ced42eb4bd1
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-03-16 19:02:07 +0900

    Add files via upload

--- diffusers/scribble2image/scribble2image.py
+++ diffusers/scribble2image/scribble2image.py
@@ -1,14 +1,19 @@
 import os 
 os.makedirs('results', exist_ok=True)
 
+import glob
 import numpy as np
 import torch
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, EulerAncestralDiscreteScheduler, AutoencoderKL
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, AutoencoderKL
 from diffusers.utils import load_image
-import controlnet_hinter
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--controlnet',
+    type=str,
+    default='controlnet/sd-controlnet-canny'
+)
 parser.add_argument(
     '--model',
     type=str,
@@ -57,10 +62,15 @@ parser.add_argument(
     help='vae'
 )
 parser.add_argument(
-    '--resolution',
-    type=int,
-    default=512,
-    help='resolution(need square image)'
+    '--prompt',
+    type=str,
+    help='prompt'
+)
+parser.add_argument(
+    '--scheduler',
+    type=str,
+    default='pndm',
+    choices=['pndm', 'multistepdpm', 'eulera']
 )
 args = parser.parse_args()
 
@@ -68,36 +78,73 @@ seed = args.seed
 steps = args.steps
 scale_list = args.scale
 
-resolution = args.resolution
-
 vae_folder =args.vae
+control_model_id = args.controlnet
 base_model_id = args.model
 
+n_samples = args.n_samples
+
 if args.from_scribble:
-    control = load_image(args.image).resize((resolution, resolution))
+    if os.path.isdir(args.image):
+        controlhint_list = glob.glob(f'{args.image}/*.png')
+        n_samples = 1
+    else:
+        controlhint_list = [args.image]
 else:
-    control = controlnet_hinter.hint_fake_scribble(
-        np.array(load_image(args.image)), 
-        width=resolution, height=resolution,
-        detect_resolution=resolution)
-    control.save(os.path.join('results', 'scribble.png'))
+    if os.path.isdir(args.image):
+        assert False, 'image argument should be a image file name, not folder'
+    else:
+        from scribble import make_scribble_image
+        controlhint_list = make_scribble_image(args.image)
+        n_samples = 1
 
 if vae_folder is not None:
-    vae = AutoencoderKL.from_pretrained(vae_folder).to('cuda')
+    vae = AutoencoderKL.from_pretrained(vae_folder, torch_dtype=torch.float16).to('cuda')
 else:
-    vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae').to('cuda')
+    vae = AutoencoderKL.from_pretrained(base_model_id, subfolder='vae', torch_dtype=torch.float16).to('cuda')
 
-controlnet = ControlNetModel.from_pretrained("basemodel/sd-controlnet-scribble")
+controlnet = ControlNetModel.from_pretrained(control_model_id, torch_dtype=torch.float16)
 
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
     base_model_id,
     controlnet=controlnet,
     vae=vae,
-    safety_checker=None).to('cuda')
-pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    safety_checker=None,
+    torch_dtype=torch.float16).to('cuda')
+
+scheduler = args.scheduler
+match scheduler:
+    case 'pmdn':
+        from diffusers import  PNDMScheduler
+        pipe.scheduler = PNDMScheduler.from_config(pipe.scheduler.config)
+    case 'multistepdpm':
+        from diffusers import DPMSolverMultistepScheduler
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    case 'eulera':
+        from diffusers import EulerAncestralDiscreteScheduler
+        pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    case _:
+        None
+
 pipe.enable_xformers_memory_efficient_attention()
 
-for i in range(args.n_samples):
+if args.prompt is not None and os.path.isfile(args.prompt):
+    print(f'reading prompts from {args.prompt}')
+    with open(args.prompt, 'r') as f:
+        prompt_from_file = f.readlines()
+        prompt_from_file = [x.strip() for x in prompt_from_file if x.strip() != '']
+        prompt_from_file = ', '.join(prompt_from_file)
+        prompt = f'{prompt_from_file}, best quality, extremely detailed'
+else:
+    prompt = 'best quality, extremely detailed'
+
+negative_prompt = 'monochrome, lowres, bad anatomy, worst quality, low quality'
+
+print(f'n_samples: {n_samples}')
+print(f'prompt: {prompt}')
+print(f'negative prompt: {negative_prompt}')
+
+for i in range(n_samples):
     seed_i = seed + i * 1000
     for scale in scale_list:
         generator = torch.manual_seed(seed_i)
@@ -105,8 +152,6 @@ for i in range(args.n_samples):
             prompt="best quality, extremely detailed", 
             negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality",
             image=control,
-            width = resolution,
-            height = resolution,
             num_inference_steps=steps, 
             generator=generator,
             guidance_scale = scale,
